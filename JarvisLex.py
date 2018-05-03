c6ca961afae1f3d952fb0d302747145bbdf3d073
Kelvin <kelvin.roche@upr.edu>
2018-05-03 14:40:39 -0400

    Added tokens and definitions for NUMBER, ID, UNKNOWN, STRING.

--- JarvisLex.py
+++ JarvisLex.py
@@ -1,6 +1,6 @@
 import ply.lex as lex
 
-#Reserved Words
+#RESERVED WORDS
 
 actions = {
     'RollDice': 'ROLLDICE',
@@ -34,6 +34,53 @@ reserved = {
 }
 
 reserved.update(actions)
+
+
+# TOKENS
+
+
+tokens = [
+    'NUMBER',
+    'UNKNOWN',
+    'ID',
+    'STRING',
+    'LP',
+    'RP',
+    'LC',
+    'RC',
+    'COMMA',
+    'SEMICOLON',
+    'COLON',
+    'ADD',
+    'QUOTES',
+    'PERIOD',
+    'PLUS',
+    'OR',
+    'AND',
+    'NOT'
+
+] + list(reserved.values())
+
+# Regular expressions rules for simple tokens
+
+#Rule for Numbers (INT)
+def t_NUMBER(t):
+    r'(\d+)'
+    return t
+
+# Rule for Variables and Reserved Words
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID') #Check for reserved words
+    return t
+
+#Rule for unknown characters
+t_UNKNOWN = r'\?'
+
+def t_STRING(t):
+    r'(\"[a-zA-Z0-9_?!@#$%&*-+().,]*\")'
+    return t
+
 
 
 
