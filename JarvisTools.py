01df386998a0d105627be490129ac29c90119885
Fernando R. Guzman Casas <fernando.guzman2@upr.edu>
2018-05-05 12:16:45 -0400

    Jarvis Tools implementation (partial)

--- JarvisTools.py
+++ JarvisTools.py
@@ -9,11 +9,20 @@ class Rule:
     def get_name(self):
         return self.name
 
-    def get_delimeter(self):
+    def get_delimiter(self):
         return self.delimiter
 
-    def get_delimeter(self):
+    def get_responses(self):
         return self.responses
+
+    def set_name(self,name):
+        self.name = name
+
+    def set_delimiter(self, delimiter):
+        self.delimiter = delimiter
+
+    def set_responses(self, responses):
+        self.responses = responses
 
 
 class Response:
@@ -23,6 +32,9 @@ class Response:
 
     def get_message(self):
         return self.message
+
+    def set_message(self, message):
+        self.message = message
 
 
 
@@ -38,3 +50,51 @@ class Variable:
     def get_value(self):
         return self.value
 
+    def set_name(self, name):
+        self.name = name
+
+    def set_value(self, value):
+        self.value = value
+
+
+class Learn:
+
+    def __init__(self, var):
+        self.var = var
+
+    def get_var(self):
+        return self.var
+
+    def set_var(self, var):
+        self.var = var
+
+
+class Forget:
+
+    def __init__(self, var):
+        self.var = var
+
+    def get_var(self):
+        return self.var
+
+    def set_var(self, var):
+        self.var = var
+
+
+class Action:
+
+    def __init__(self, action, parameter):
+        self.action = action
+        self.parameter = parameter
+
+    def get_action(self):
+        return self.action
+
+    def get_parameter(self):
+        return self.parameter
+
+    def set_action(self, action):
+        self.action =action
+
+    def set_parameter(self, parameter):
+        self.parameter = parameter
