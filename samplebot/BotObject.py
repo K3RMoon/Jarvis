abdd8dba441ae1ae20ea22787ba19b6910149515
Herbert <herbert.perez@upr.edu>
2018-05-08 19:47:10 -0400

    added a lot of comments.
    fixed small issue related to validation when parameters have names other than ~param

--- samplebot/BotObject.py
+++ samplebot/BotObject.py
@@ -24,6 +24,10 @@ class bot:
     #so we need to tokenize it somehow, separate a specific symbol, like (%) for now [could and maybe should be changed]
     # in order to validate, go through all lists and check if equal
     # the symbol to use is now (~) because (%) is already used by python I guess
+
+
+    #how does this work? it depends heavily on the ammount of arguments. as in, the ammount of tokens basically
+    #it goes comparing token by token, if all (aka the size of ruleList) match, good. once any doesnt match resets to 0.
     def checkRule(self, rule, ruleList, params): #by Herbert, will try to do the new verification here
         trues = 0
         cindex2=-1
@@ -41,7 +45,9 @@ class bot:
                 else:
                     if(ruleList[cindex2][cindex][0]=='~'):
                         params.append(x)
-                        rule[cindex] = "~param"
+                        #rule[cindex] = "~param"
+                        # Quick fix to allos other parameter names!
+                        rule[cindex] = ""+ruleList[cindex2][cindex]
                     trues+=1
                     #print(trues)
                     if (trues== rule.__len__()):
@@ -104,7 +110,7 @@ class bot:
         if(rule[0].lower()=="action"):
             if(rule[1].lower()=="sum"):
                 #WARNING: maybe will also have to verify that we have the correct ammount of arguments?
-                    #tho maybe that should be verified in the yacc. we'll see. 
+                    #tho maybe that should be verified in the yacc. we'll see.
                 if not(self.RepresentsInt(params[0]) and self.RepresentsInt(params[1])):
                     print("Illegal Argument Type")
                 else:
@@ -133,9 +139,47 @@ b.addRule("Hey","Response","Hello")
 b.addRule("How are u?", "Response","I'm doing just fine")
 b.addRule("Learn something", "Learn", "What's should i learn")
 #b.addRule("Add 2 numbers", "Action", "SUM") #outdated sum, no arguments
-b.addRule("Add 2 numbers ~param ~param", "Action", "SUM")
+b.addRule("Add 2 numbers ~num1 ~num2", "Action", "SUM")
 b.forget("name")
 
+'''''
+So...what Im thinking es tener ademas de los tipos que ya tenemos de "Response/Action/Learn", anadir uno que sea combination.
+    *Justo despues de combination tiene el tipo de combination que es (Si hacemos AND y OR, si solo hacemos AND pichea eso)
+    *despues de esto tenemos un numero, es la cantidad de cosas que se combinan
+    *finally tenemos las cosas themselves, following el formato normal de "type:response"
+
+"This is the user input ~NUM1 ~NUM2" "Combination" "3" "AND" "ACTION" "SUM" "Response" "doing number operations" "ACTION" "substract"
+
+    this could work, but it would take quite a bit of time? I dunno.
+-----------------------------------/////////----------------------------///////////--------------------------/////////-----------
+Otra alternativa es anadirlos como sub comandos, y que se llamen cada uno. Like, idk, 
+
+Si el usuario espera que el input "Do stuff" tire tres respuestas, el parser genera 
+
+"Do stuff 1", "response", "blah"
+"Do stuff 2", "response", "stuff"
+"Do stuff 3", "response", "hi"
+
+y corre las tres somehow. or something similar. 
+
+pero no me gusta tanto esta idea pque either leads to confusion o causa issues al validar, cause people also might want to have
+numbers in the title from the beginning sooo...pichea, me gusta mas la primera idea.
+-----------------------------------/////////----------------------------///////////--------------------------/////////-----------
+
+maybe anadir todas con el mismo nombre, y que si el programa detecta mas de una cosa con el mismo nombre, que corra todas? so poner AND
+en el codigo que nosotros developed implica en el de Python ponerlo varias veces con el mismo nombre como rule? maybe?
+
+"Do stuff", "response", "blah"
+"Do stuff", "response", "stuff"
+"Do stuff", "response", "hi"
+
+>Do stuff
+>>blah
+>>stuff
+>>hi
+
+
+'''''
 
 l=""
 while not(l.lower() == "quit"):
